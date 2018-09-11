067fa96ffede15b5b98766455d5476430db6a4e2
Steve Korzinetzki <steve.korzinetzki@lise.de>
2018-09-11 14:22:39 +0200

    step 6: showing specflow with selenium for frontend testing

---
+++ specflow-livedemo-specs/ScrumTraining.feature
@@ -0,0 +1,11 @@
+﻿Feature: Scrum Training
+	In order to book scrum trainings
+	As an interested customer
+	I want to see the scrum training information
+
+@mytag
+Scenario: Navigating to Scrum Training landing page
+	Given I am on the homepage
+	When I click on the menu
+	And I click on scrum trainings
+	Then I should be navigated to "scrum-trainings-koeln"
---
+++ specflow-livedemo-specs/ScrumTrainingSteps.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace specflow_livedemo_specs
+{
+    [Binding]
+    public class ScrumTrainingSteps
+    {
+        private static IWebDriver _driver;
+
+        [BeforeFeature()]
+        public static void BeforeFeature()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        [AfterFeature()]
+        public static void AfterFeature()
+        {
+            _driver.Quit();
+        }
+
+        [BeforeStep()]
+        public void BeforeStep()
+        {
+            System.Threading.Thread.Sleep(3000);
+        }
+
+        [Given(@"I am on the homepage")]
+        public void GivenIAmOnTheHomepage()
+        {
+            _driver.Navigate().GoToUrl("https://www.lise.de");
+        }
+        
+        [When(@"I click on the menu")]
+        public void WhenIClickOnTheMenu()
+        {
+            _driver.FindElement(By.CssSelector("#page-header > div > div.col-md-11 > button.menu-btn")).Click();
+        }
+        
+        [When(@"I click on scrum trainings")]
+        public void WhenIClickOnScrumTrainings()
+        {
+            IWebElement element = _driver
+                .FindElement(By.CssSelector("div.top > div > ul > li.pushy-submenu.pushy-submenu-open"))
+                .FindElement(By.LinkText("SCRUM TRAININGS"));
+
+            Assert.AreEqual("SCRUM TRAININGS", element.Text.Trim());
+            element.Click();
+        }
+        
+        [Then(@"I should be navigated to ""(.*)""")]
+        public void ThenIShouldBeRedirectTo(string expectedUrlPath)
+        {
+            Assert.IsTrue(_driver.Url.TrimEnd('/').EndsWith(expectedUrlPath));
+        }
+    }
+}
